bec650ac7013d522754575d63311ee95c44001c4
whitetiger100 <christianberger@hamburg.de>
2015-10-01 19:30:09 +0200

    01.10.2015

--- Ruby/Code/loesung_aufgabe01.rb
+++ Ruby/Code/loesung_aufgabe01.rb
@@ -8,12 +8,13 @@ puts "\n\tPlayers:\n\t\tlarry\n\t\tcurly\n\t\tmoe"
 n1 = "Larry"
 n2 = "Curly"
 n3 = "Moe"
+n4 = "Shemp"
 puts "\n"
 puts n1+" has a health of "+health.to_s+"."
 puts n2+" has a health of 125."
 status = "Moe has a health of 100."
 puts status.center(50, "*")
 puts status.center(50, "*")
-puts "Shemp......................... 90 health\nShemp......................... 90 health"
+puts "#{n4.capitalize.ljust(30,'.')} 90 health\n#{n4.capitalize.ljust(30,'.')} 90 health"
 puts "Players:\n\t"+n1+"\n\t"+n2+"\n\t"+n3
 puts "The game started on #{Time.new}"
